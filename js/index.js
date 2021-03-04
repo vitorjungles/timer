45fb9dd74b1326f193bfdead3505763092536e35
Vítor Jungles <vitorcamargojungles@gmail.com>
2021-03-04 18:43:03 -0300

    Version 1.0

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-var a = new Date;
-a = a.getFullYear();
-document.getElementById("year").innerHTML = a;
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+var a=''+new Date().getHours()+':'+new Date().getMinutes();
+
+document.getElementById("inicial").onclick = function() {
+  console.log(document.getElementById("time").value)
+  console.log(a);
+  while (document.getElementById("time").value!=a) {
+    a=''+new Date().getHours()+':'+new Date().getMinutes()
+  };
+  alert('Tempo atingido! ⏰');
+};
