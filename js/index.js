716e8540ed9bb7b6b192b68d102e6238be081345
Vítor Jungles <vitorcamargojungles@gmail.com>
2021-03-13 11:25:06 -0300

    Version 3.0

--- js/index.js
+++ js/index.js
@@ -1,30 +1,42 @@
 // Exchanging year text
 document.getElementById("year").textContent = new Date().getFullYear();
 
+// Permission variable
+var v=true;
 
+// Exchange text and color function
+function exchange(variable, text, color='black') {
+  variable.style.color=color;
+  variable.textContent=text;
+  return variable;
+}
+
+// Button click function
 document.getElementById("inicial").onclick = function() {
-  var elem=original=document.getElementById("time").value;
-  if (document.getElementById("alert").textContent!='Enter the desired time below:') {
-    document.getElementById("alert").textContent = 'Enter the desired time below:';
-    document.getElementById("alert").style.color = 'black';
-  };
-  if (elem!='') {
-    var interval = setInterval(e, 1000);
-    function e() {
-      elem = elem[3]=='0' ? `${elem[0]}${elem[1]}${elem[2]}${elem[4]}`: elem;
-      var ms = new Date().getHours()+':'+new Date().getMinutes()+':'+new Date().getSeconds();
-      var m = new Date().getHours()+':'+new Date().getMinutes();
-      var re = parseInt(elem.replace(/:/g, ''))-parseInt(m.replace(/:/g, ''));
-      document.getElementById("count").textContent = 'Time left: '+re;
-      console.log([elem, m, ms])
-      if (elem==m) {
-        clearInterval(interval);
-        document.getElementById("history").textContent=='----' ? document.getElementById("history").textContent = `${document.getElementById("history").textContent.split(':').length-1+1} - ${original}` : document.getElementById("history").textContent += ` ${document.getElementById("history").textContent.split(':').length-1+1} - ${original}`;
-        document.getElementById("time").value='';
-      };
+  if (v) {
+    var elem=original=document.getElementById("time").value;
+    if (document.getElementById("alert").textContent!='Enter the desired time below:') {
+      exchange(document.getElementById("alert"), 'Enter the desired time below:');
     };
-  } else {
-    document.getElementById("alert").style.color = 'red';
-    document.getElementById("alert").textContent = 'Enter the desired time below, please:';
+    if (elem!='') {
+      v=false;
+      var interval = setInterval(e, 1000);
+      function e() {
+        elem = elem[3]=='0' ? `${elem[0]}${elem[1]}${elem[2]}${elem[4]}`: elem;
+        var ms = new Date().getHours()+':'+new Date().getMinutes()+':'+new Date().getSeconds();
+        var m = new Date().getHours()+':'+new Date().getMinutes();
+        var re = parseInt(elem.replace(/:/g, ''))-parseInt(m.replace(/:/g, ''));
+        document.getElementById("count").textContent = 'Time left: '+re;
+        console.log([elem, m, ms])
+        if (elem==m) {
+          clearInterval(interval);
+          v=true;
+          document.getElementById("history").textContent=='----' ? document.getElementById("history").textContent = `${document.getElementById("history").textContent.split(':').length-1+1} - ${original}` : document.getElementById("history").textContent += ` ${document.getElementById("history").textContent.split(':').length-1+1} - ${original}`;
+          document.getElementById("time").value='';
+        };
+      };
+    } else {
+      exchange(document.getElementById("alert"), 'Enter the desired time below, please:', 'red');
+    };
   };
 };
