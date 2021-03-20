ea9e59abd08ee6ba0bdf410803fc44f0d175ba89
Vítor Jungles <vitorcamargojungles@gmail.com>
2021-03-19 21:04:33 -0300

    More updates

--- js/index.js
+++ js/index.js
@@ -3,6 +3,22 @@ var year = document.createElement("span");
 year.textContent = ` ${new Date().getFullYear()}`;
 document.getElementById("copyright").after(year);
 
+// Permission variable
+var verify = true;
+
+// Input validation
+function validate() {
+  if (isNaN(document.getElementById("minutes").value)) {
+    document.getElementById("minutes").value = '';
+  };
+  return isNaN(document.getElementById("minutes").value) ? false : true;
+};
+$(function() {
+  $("input[name='data']").on('input', function(e) {
+    $(this).val($(this).val().replace(/[^0-9]/g, ''));
+  });
+});
+
 // Exchange text and color function
 function exchange(variable, text, color='black') {
   variable.style.color=color;
@@ -15,13 +31,15 @@ document.getElementById("inicial").onclick = function() {
   var minutes = document.querySelectorAll("input").item(0);
   var seconds = document.querySelectorAll("input").item(1);
   var TimeSectionDiv = document.querySelector("div");
-  var TimeSection = document.querySelector("section");
   if (document.getElementById("alert").textContent!='Enter the desired time below:') {
     exchange(document.getElementById("alert"), 'Enter the desired time below:');
   };
-  if (((minutes.value!='00' && seconds.value!='00') || (minutes.value!='00' && seconds.value=='00') || (minutes.value=='00' && seconds.value!='00')) && (isNaN(minutes.value)==false && isNaN(seconds.value)==false)) {
+  if ((minutes.value!='00' || seconds.value!='00') && verify) {
     TimeSectionDiv.hidden=true;
+    verify=false;
     var Count = document.createElement("h1");
+    var Button = document.getElementById("inicial");
+    Button.value = 'Pause';
     Count.textContent = minutes.value+':'+seconds.value;
     TimeSectionDiv.before(Count);
   } else {
